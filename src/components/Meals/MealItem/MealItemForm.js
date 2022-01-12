b48b26f3f231c24a1d76c4b8e553144e76eb317f
MYkola Batsvin <mykola.batsvin@gmail.com>
2022-01-12 16:52:04 +0200

    finished Building a Food Order App

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,26 @@
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
+import { useRef, useState } from 'react';
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label='Amount' input={{
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false)
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    }
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input ref={amountInputRef} label='Amount' input={{
             id: 'amount',
             type: 'number',
             min: '1',
@@ -12,6 +29,7 @@ const MealItemForm = props => {
             defaultValue: '1',
         }} />
         <button>Add</button>
+        {!amountIsValid && <p>Please enter a valid amout (1-5.)</p>}
     </form>
 
 }
